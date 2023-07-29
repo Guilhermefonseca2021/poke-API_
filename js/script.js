e88b3047a3242a39f9c19acce0ca0c06d6e6990b
Guilherme Fonseca <gsbloogs194@gmail.com>
2023-07-29 13:43:52 -0300

    .

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@ let changeThemeBtn = document.querySelector("#change-theme")
 
 changeThemeBtn.addEventListener('click', () => {
     // acesso o document p body e vejo a lista e dispao "toggle" dark meu tema para lista de classes
+    let header = document.querySelector('header')
+
     document.body.classList.toggle("light-theme")
+    header.classList.toggle('light-theme')
 });
+
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+    const data = await APIResponse.json();
+    return data;
+}
+
+fetchPokemon('25');
