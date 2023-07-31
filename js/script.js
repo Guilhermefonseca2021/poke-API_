234084cd6ee06fedb313384c5f8fd371957da0d2
Guilherme Fonseca <gsbloogs194@gmail.com>
2023-07-30 22:10:28 -0300

    api introduce

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let changeThemeBtn = document.querySelector("#change-theme")
-
+let namePokemon = document.querySelector('.name')
+let numberPokemon = document.querySelector('.number') 
+// dark-theme
 changeThemeBtn.addEventListener('click', () => {
     // acesso o document p body e vejo a lista e dispao "toggle" dark meu tema para lista de classes
     let header = document.querySelector('header')
@@ -11,9 +13,19 @@ changeThemeBtn.addEventListener('click', () => {
 
 const fetchPokemon = async (pokemon) => {
 
-    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon);
     const data = await APIResponse.json();
     return data;
 }
 
-fetchPokemon('25');
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    namePokemon.innerHTML = data.name;
+    numberPokemon.innerHTML = '#' + data.id;
+}
+
+renderPokemon('26');
+
+console.log(numberPokemon)
