734b497a2426541a8f14044cc5d47faf3b7de9a3
Guilherme Fonseca <gsbloogs194@gmail.com>
2023-08-02 16:39:37 -0300

    closed to finish

--- js/script.js
+++ js/script.js
@@ -13,74 +13,28 @@ changeThemeBtn.addEventListener('click', () => {
 // pokemon list
 let namePokemon = document.querySelector('.name')
 let numberPokemon = document.querySelector('.number') 
-const pokeContainer = document.querySelector('.pokemon')
+let imgPokemon = document.querySelector('.imgPokemon')
+// const pokeContainer = document.querySelector('.pokemon')
 const pokemonCount = 150;
-const pokemonType = Object.keys(colorType);
-const colorType = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#F5F5F5'
-}
+
 
 
 const fetchPokemon = async (pokemon) => {
-
     const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon);
     const data = await APIResponse.json();
     return data;
 
     console.log(data)
-    // const pokemonPromises = []
 }
 
 const renderPokemon = async (pokemon) => {
 
-    const data = await fetchPokemon(pokemon)
-    
-    for(index = 1; index < pokemonCount; index++) {
-        await APIResponse(index)
-    }
+    const data = await fetchPokemon(pokemon);
 
     namePokemon.innerHTML = data.name;
     numberPokemon.innerHTML = '#' + data.id;
-}
+    imgPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
-const createPokemonCard = (pokemon) => {
-    const card = document.createElement('div')
-    card.classList.add('pokemon')
-
-    const name = pokemon.name[0].toUpperCase() + pokemonCount.name.slice[1]
-    const id = pokemon.id.toString().padStart(3, '0')
-
-    const pokemonType = pokemon.types.map(type => type.type.name)
-    const type = mainTypes.find(type => pokemonType.indexOf(type) > -1)
-    const color = colorType(type)
-
-    card.style.backgroundColor = color;
-
-    const pokemonInnerHTML = ´
-    <div class="pokemon">
-    <div class="imgContainer">
-        <img src="./pokemon/001.png" alt="nome">
-    </div>
-    <div class="info">
-        <span class="number">#01</span>
-        <h3 class="name">Charmander</h3>
-        <small class="type">type: <span>Fogo</span></small>
-    </div>
-    </div>   
-´
 }
 
 
